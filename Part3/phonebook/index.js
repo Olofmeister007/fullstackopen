524c1270a1cb6a5df369fb44731544a91d846393
Olofmeister911 <danyalraxa007@gmail.com>
2026-04-30 19:22:08 +0500

    uibuild

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -1,44 +1,47 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(express.static('dist'))
 
-
-var morgan = require('morgan')
+var morgan = require("morgan");
 app.use(cors());
 app.use(express.json());
-morgan.token('posttt', (req,res) => {
-    if(req.method === 'POST')
-    {
-        return JSON.stringify(req.body)
-    }
-})
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :posttt'))
+morgan.token("posttt", (req, res) => {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body);
+  }
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :posttt",
+  ),
+);
 
-app.get('/', function (req, res) {
-  res.send('hello, world!')
-})
+app.get("/", function (req, res) {
+  res.send("hello, world!");
+});
 
 let data = [
   {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456',
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345',
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
   {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
   },
 ];
 
@@ -51,17 +54,17 @@ let data = [
 // }
 
 // app.use(requestLogger);
-app.get('/api/persons', (req, res) => {
+app.get("/api/persons", (req, res) => {
   res.send(data);
 });
 
-app.get('/info', (req, res) => {
+app.get("/info", (req, res) => {
   res.send(
     `Phonebook has info for ${data.length} people <br><br>${new Date()}`,
   );
 });
 
-app.get('/api/persons/:id', (req, res) => {
+app.get("/api/persons/:id", (req, res) => {
   const { id } = req.params;
   // console.log(id)
 
@@ -73,26 +76,26 @@ app.get('/api/persons/:id', (req, res) => {
   res.send(personData);
 });
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete("/api/persons/:id", (req, res) => {
   const { id } = req.params;
 
   data = data.filter((person) => person.id !== id);
   res.status(204).send();
 });
 
-app.post('/api/persons', (req, res) => {
+app.post("/api/persons", (req, res) => {
   const person = req.body;
 
   if (!person.name || !person.number) {
-    return res.status(400).send('Enter name and number for user');
+    return res.status(400).send("Enter name and number for user");
   }
   const found = data.find((ele) => ele.name === person.name);
   if (found) {
-    return res.status(400).json({ error: 'name must be unique' });
+    return res.status(400).json({ error: "name must be unique" });
   }
 
   const personObject = {
-    id: String(Math.ceil((Math.random() * 1000))),
+    id: String(Math.ceil(Math.random() * 1000)),
     name: person.name,
     number: person.number,
   };
@@ -103,10 +106,10 @@ app.post('/api/persons', (req, res) => {
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
 const PORT = 3003;
 app.listen(PORT, () => {
